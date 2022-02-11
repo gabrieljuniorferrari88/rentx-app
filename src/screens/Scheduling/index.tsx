db83587d20f2b09b68dd7a1007c1aa4de1281987
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-02-11 20:03:57 -0300

    instalando a biblioteca e personalizando o reactve-native-calendars

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { useTheme } from 'styled-components/native';
 
+import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 import { BackButton } from '../../components/BackButton';
 
 import ArrowSvg from '../../assets/arrow.svg';
@@ -17,7 +19,6 @@ import {
 	Content,
 	Footer,
 } from './styles';
-import { Button } from '../../components/Button';
 
 export function Scheduling(){
 	const theme = useTheme();
@@ -57,7 +58,7 @@ export function Scheduling(){
 			</Header>
 
 			<Content>
-
+				<Calendar />
 			</Content>
 
 			<Footer>
