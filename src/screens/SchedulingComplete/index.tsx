d6cd1f1c8726f83fff8e8ba74ee379f0ee030db5
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-02-14 10:05:39 -0300

    estilizando a página de completo

--- src/screens/SchedulingComplete/index.tsx
+++ src/screens/SchedulingComplete/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
 import { useWindowDimensions } from 'react-native';
 
 import LogoSvg from '../../assets/logo_background_gray.svg';
 import DoneSvg from '../../assets/done.svg';
 
 import {
-	Container
+	Container,
+	Content,
+	Title,
+	Message,
 } from './styles';
 
 export function SchedulingComplete(){
+	const { width } = useWindowDimensions();
+
 	return (
 		<Container>
-			<LogoSvg />
+			<LogoSvg width={width}/>
+
+			<Content>
+				<DoneSvg height={RFValue(80)} width={RFValue(80)} />
+				<Title>Carro Alugado!</Title>
+
+				<Message>
+					Agora você só precisa ir {'\n'}
+					até a concessionaria da RENTEX {'\n'}
+					pegar o seu automóvel
+				</Message>
+			</Content>
+
 		</Container>
 	);
 }
