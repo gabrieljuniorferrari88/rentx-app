8bce303c39a104bca261c4ccd7362d6e741f4b28
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-02-23 18:32:42 -0300

    configurando trabalhando com animacao no slide

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
 	Container,
@@ -17,7 +17,19 @@ interface Props {
 	// }[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props){
+	const [imageIndex, setImageIndex] = useState(0);
+	
+	const indexChanged = useRef((info: ChangeImageProps) => {
+		const index = info.viewableItems[0].index!;
+		setImageIndex(index);
+	});
+
 	return (
 		<Container>
 			
@@ -26,7 +38,7 @@ export function ImageSlider({ imagesUrl }: Props){
 					imagesUrl.map((_, index) => (
 						<ImageIndex 
 							key={String(index)}
-							active={true} 
+							active={index === imageIndex} 
 						/>
 					))
 				}
@@ -42,7 +54,7 @@ export function ImageSlider({ imagesUrl }: Props){
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
-        // onViewableItemsChanged={indexChanged.current}
+        onViewableItemsChanged={indexChanged.current}
         decelerationRate="fast"
       />
 
