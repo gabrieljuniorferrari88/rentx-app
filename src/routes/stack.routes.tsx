b49bde5112534899a483ef06705ebc2aa668fd5c
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-03-03 19:46:07 -0300

    .

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,18 +8,20 @@ import { Scheduling } from '../screens/Scheduling/index';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
+import { Splash } from '../screens/Splash';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes(){
 	return(
-		<Navigator screenOptions={{ headerShown: false }}>
-			<Screen name="Home" component={Home} />
-			<Screen name="CarDetails" component={CarDetails} />
-			<Screen name="Scheduling" component={Scheduling} />
-			<Screen name="SchedulingDetails" component={SchedulingDetails} />
-			<Screen name="SchedulingComplete" component={SchedulingComplete} />
-			<Screen name="MyCars" component={MyCars} />
+		<Navigator screenOptions={{ headerShown: false }} initialRouteName='Splash'>
+			<Screen name='Splash' component={Splash} />
+			<Screen name='Home' component={Home} />
+			<Screen name='CarDetails' component={CarDetails} />
+			<Screen name='Scheduling' component={Scheduling} />
+			<Screen name='SchedulingDetails' component={SchedulingDetails} />
+			<Screen name='SchedulingComplete' component={SchedulingComplete} />
+			<Screen name='MyCars' component={MyCars} />
 		</Navigator>
 	)
 }
--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { StatusBar, Button, StyleSheet } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 import {
 	Container
@@ -7,7 +9,23 @@ import {
 export function Splash(){
 	return (
 		<Container>
+			<StatusBar 
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
 
+			<Animated.View style={styles.box}/>
+
+			<Button title='Mover' onPress={()=> {}}/>
 		</Container>
 	);
 }
+
+const styles = StyleSheet.create({
+	box: {
+		width: 100,
+		height: 100,
+		backgroundColor: 'red'
+	}
+})
