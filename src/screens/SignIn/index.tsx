00cb38c53f3664d6781c355014618976ebe201f4
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-03-31 18:05:57 -0300

    adjusting keyboard usability with TouchableWithoutFeedback

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {
 	StatusBar,
-	KeyboardAvoidingView
+	KeyboardAvoidingView,
+	TouchableWithoutFeedback,
+	Keyboard
 } from 'react-native';
 import { useTheme } from 'styled-components';
 
@@ -23,48 +25,50 @@ export function SignIn(){
 
 	return (
 		<KeyboardAvoidingView behavior='position' enabled>
-			<Container>
-				<StatusBar 
-					barStyle='dark-content'
-					backgroundColor='transparent'
-					translucent
-				/>
-				<Header>
-					<Title>Estamos{'\n'}quase lá.</Title>
-					<SubTitle>Faça seu login para começar{'\n'} uma experiência incrível.</SubTitle>
-				</Header>
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+				<Container>
+					<StatusBar 
+						barStyle='dark-content'
+						backgroundColor='transparent'
+						translucent
+					/>
+					<Header>
+						<Title>Estamos{'\n'}quase lá.</Title>
+						<SubTitle>Faça seu login para começar{'\n'} uma experiência incrível.</SubTitle>
+					</Header>
 
-				<Form>
-					<Input
-						iconName='mail' 
-						placeholder='E-mail'
-						keyboardType='email-address'
-						autoCorrect={false}
-						autoCapitalize='none'
-					/>
-					<PasswordInput
-						iconName='lock' 
-						placeholder='Senha'
-					/>
-				</Form>
+					<Form>
+						<Input
+							iconName='mail' 
+							placeholder='E-mail'
+							keyboardType='email-address'
+							autoCorrect={false}
+							autoCapitalize='none'
+						/>
+						<PasswordInput
+							iconName='lock' 
+							placeholder='Senha'
+						/>
+					</Form>
 
-				<Footer>
-					<Button 
-						title='Login'
-						onPress={() => {}}
-						enabled={false}
-						loading={false}
-					/>
-					<Button 
-						title='Criar conta gratuita'
-						onPress={() => {}}
-						enabled={true}
-						loading={false}
-						color={theme.colors.background_secondary}
-						light
-					/>
-				</Footer>
-			</Container>
+					<Footer>
+						<Button 
+							title='Login'
+							onPress={() => {}}
+							enabled={false}
+							loading={false}
+						/>
+						<Button 
+							title='Criar conta gratuita'
+							onPress={() => {}}
+							enabled={true}
+							loading={false}
+							color={theme.colors.background_secondary}
+							light
+						/>
+					</Footer>
+				</Container>
+			</TouchableWithoutFeedback>
 		</KeyboardAvoidingView>
 	);
 }
